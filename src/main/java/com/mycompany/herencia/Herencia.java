8fce0105422dbab8f9df848a88687fa549fec0e2
darwinn0 <dfguzman@uth.hn>
2024-11-09 15:17:18 -0600

    Actualizacion de la clase perro

--- src/main/java/com/mycompany/herencia/Herencia.java
+++ src/main/java/com/mycompany/herencia/Herencia.java
@@ -8,14 +8,27 @@ package com.mycompany.herencia;
  *
  * @author darwi
  */
+import java.util.ArrayList;
+import java.util.List;
 public class Herencia {
 
     public static void main(String[] args) 
     {
         Perro p = new Perro("Color negro","SI","SI","SI",4);
-        p.Sonido();
+        //p.Sonido();
         
         Gato g = new Gato("Blanco","SI","SI","SI",4);
-        g.Sonido();
+        //g.Sonido();
+        
+        List<Animal> lista = new ArrayList<Animal>();
+        lista.add(p);
+        lista.add(g);
+        
+        lista.add(new Queco ("NO","SI","SI","SI",4));
+        
+        for (Animal item : lista)
+        {
+            item.Sonido();
+        }
     }
 }
