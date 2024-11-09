0a07202102cfe38e39d81e0a95851cba0d6d7853
darwinn0 <dfguzman@uth.hn>
2024-11-09 14:02:02 -0600

    Actualizacion de la clase perro

---
+++ src/main/java/com/mycompany/herencia/Herencia.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herencia;
+
+/**
+ *
+ * @author darwi
+ */
+public class Herencia {
+
+    public static void main(String[] args) 
+    {
+        Perro p = new Perro("Color negro","SI","SI","SI",4);
+        p.Sonido();
+        
+        Gato g = new Gato("Blanco","SI","SI","SI",4);
+        g.Sonido();
+    }
+}
